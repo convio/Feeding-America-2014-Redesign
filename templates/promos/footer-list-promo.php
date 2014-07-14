3f19697e62b2112c7f80d34204e7ac3199a012d5
elizabethm <elizabeth.makayed@dnlomnimedia.com>
2014-07-14 14:59:29 -0700

    updated footer promos to use headline field

---
+++ templates/promos/footer-list-promo.php
@@ -0,0 +1,25 @@
+<t:if test="length > 0">
+  <t:list>
+    <t:set id="buttonClasses" value="'button'" />
+    <t:if test="action_button_color.length > 0 || action_button_style.length > 0">
+      <t:if test="action_button_style.length > 0">
+        <t:list id="action_button_style">
+          <t:set id="buttonClasses" value="name" />
+        </t:list>
+      </t:if>
+      <t:if test="action_button_color.length > 0">
+        <t:list id="action_button_color">
+          <t:set id="buttonClasses" value="concat(concat(buttonClasses, ' '), name)" />
+        </t:list>
+      </t:if>
+    </t:if>
+    <div class="footer-promo">
+      <t:if test="thumb.length > 0"><t:list id="thumb"><img src="${url}" alt="${alt_text}" /></t:list></t:if>
+      <t:if test="!isNull(headline)"><div class="promo-title">${headline}</div></t:if>
+      <t:if test="!isNull(description)"><div class="promo-text">${description}</div></t:if>
+      <t:if test="!isNull(body)"><div class="promo-action">${body}</div></t:if>
+      <t:if test="!isNull(action_button_label) && !isNull(action_button_url)"><div class="promo-action"><a class="${buttonClasses}" href="${action_button_url}">${action_button_label}</a></div></t:if>
+
+    </div>
+  </t:list>
+</t:if>
